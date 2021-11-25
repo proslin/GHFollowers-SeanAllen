f40e26e146219f03563bea5469c19373ee05d993
Prosvetova Lina <linchik@mail.ru>
2021-11-25 12:19:42 +0300

    Initial commit

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Prosvetova Lina on 23.11.2021.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+    
+
+}
--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  GHFollowers
 //
 //  Created by Prosvetova Lina on 23.11.2021.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+      
     }
+    
 
-
+   
 }
-
