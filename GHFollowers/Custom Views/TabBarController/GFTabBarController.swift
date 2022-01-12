fa064c0e95dbbb72ee67ce28f0ff7a3160c59218
Prosvetova Lina <linchik@mail.ru>
2022-01-12 13:27:35 +0300

    Optimization. UITabBarController

---
+++ GHFollowers/Custom Views/TabBarController/GFTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  GFTabBarController.swift
+//  GHFollowers
+//
+//  Created by Prosvetova Lina on 12.01.2022.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNC(), createFavoritesNC()]
+        
+    }
+    
+    func createSearchNC() -> UINavigationController {
+      let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    
+    func createFavoritesNC() -> UINavigationController {
+        let favoritesListVC = FavoritesListVC()
+        favoritesListVC.title = "Favorites"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 0)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+     
+}
